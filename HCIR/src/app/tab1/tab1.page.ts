9b30d4aab1aee369960b57ee89cd01f0fe89f300
Johannes <johannes.bluhm@outlook.com>
2022-11-06 15:06:28 +0100

    tone and eval

--- HCIR/src/app/tab1/tab1.page.ts
+++ HCIR/src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import * as Tone from 'tone'
+import {RangeCustomEvent} from "@ionic/angular";
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,16 +10,78 @@ import * as Tone from 'tone'
 })
 export class Tab1Page {
   synth: any;
+  sliderValue: number;
+  currentNote: any
 
-  constructor() {
+  /*
+  Octave 4
+  See https://mixbutton.com/mixing-articles/music-note-to-frequency-chart/
+   */
+
+  notes: object = {
+    C: 261.63,
+    D: 293.66,
+    E: 329.63,
+    F: 349.23,
+    G: 392.00,
+    A: 440.00,
+    H: 493.88
+  }
+
+  steps: any
+
+  constructor(private alertController: AlertController) {
     this.synth = new Tone.Synth().toDestination();
+    this.setup()
   }
 
-  playSound() {
-    console.log('playing sound')
 
-//play a middle 'C' for the duration of an 8th note
-    this.synth.triggerAttackRelease("C4", "8n");
+  playReferenceSound() {
+    this.synth.triggerAttackRelease(this.currentNote, "8n");
   }
 
+  playSelectedSound() {
+    this.synth.triggerAttackRelease(this.steps[this.sliderValue], "8n");
+  }
+
+  async confirmSelection() {
+    console.log(this.currentNote)
+    console.log(this.steps[this.sliderValue])
+    const diff = Math.abs(this.currentNote - this.steps[this.sliderValue])
+
+    const alert = await this.alertController.create({
+      header: 'Ergebnis',
+      message: 'Die Differenz beträgt: ' + diff,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+    await alert.onDidDismiss();
+    this.setup();
+  }
+
+  onSliderChange(ev: Event) {
+    this.sliderValue = <number>(ev as RangeCustomEvent).detail.value;
+  }
+
+  setup() {
+    this.sliderValue = 5;
+    const oldNote = this.currentNote
+
+    do {
+      this.currentNote = getRandomProperty(this.notes)
+    } while (oldNote == this.currentNote)
+
+    console.log(this.currentNote)
+
+    this.steps = []
+    for (let i = 0; i < 10; i++) {
+      this.steps[i] = this.currentNote + (i * 10 - 50);
+    }
+  }
+}
+
+function getRandomProperty(obj) {
+  const keys = Object.keys(obj);
+  return obj[keys[keys.length * Math.random() << 0]];
 }
