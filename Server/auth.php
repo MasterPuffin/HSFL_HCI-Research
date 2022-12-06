fa01b10f4fcb39fe949e29dd6078e090dc0c3613
Johannes <johannes.bluhm@outlook.com>
2022-12-06 19:25:39 +0100

    add missing headers

--- Server/auth.php
+++ Server/auth.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
 header('Content-Type: application/json; charset=utf-8');
 
 if (API_KEY != @$_GET['key']) die('{"success": false, "reason": "wrong api key"}');
