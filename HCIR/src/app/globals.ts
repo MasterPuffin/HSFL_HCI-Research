25698deb8f8d10ddecf85ec3973a3a0175344ace
Johannes <johannes.bluhm@outlook.com>
2022-12-13 22:20:49 +0100

    added server

--- HCIR/src/app/globals.ts
+++ HCIR/src/app/globals.ts
@@ -1,4 +1,5 @@
 export const GlobalVariables = Object.freeze({
   BASE_API_URL: 'http://localhost/hcir/',
+  // BASE_API_URL: 'https://hsfl.ucdn.de/hcir/server/',
   API_KEY: 'TSmE7UFRABhf44WaVGkqnCN2en'
 });
